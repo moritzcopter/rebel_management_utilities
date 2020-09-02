a7e8c2f68887c6165e50767c5e1f33b49258e892
Martijn Brehm <martijn@brehm.nl>
2020-09-02 14:51:39 +0200

    Fixed a bug where the script crashed if a telegram user deleted hides their phone number from us.

--- ag_channels/sync_ag_channels.py
+++ ag_channels/sync_ag_channels.py
@@ -15,7 +15,6 @@ import os
 regions = ["Midden", "Zuid-Oost", "Zuid-Holland", "Noord-Oost", "Noord-West"]
 logs = {}
 
-
 def convert_phone_number(phonenumber):
     """
         Replace 06 in phonenumbers with +316, to work with the telegram API.
@@ -64,9 +63,12 @@ def get_all_ags(api_key, an_ag_endpoints):
 
         # Try to include the rep's name - some people haven't filled this in.
         try:
-            content["custom_fields"].update({"rep_name" : content["given_name"]})
+            if content["given_name"] != 'test': # Multiple people had 'test' as name...
+                content["custom_fields"].update({"rep_name" : content["given_name"]})
+            else:
+                content["custom_fields"].update({"rep_name" : "rebel"})
         except KeyError:
-            pass
+            content["custom_fields"].update({"rep_name" : 'rebel'})
         ags.append(content["custom_fields"])
     return ags
 
@@ -80,11 +82,18 @@ async def sync_channels(api_key, links, super_admins, an_ag_endpoints):
     for region in regions:
 
         # Get the AG's which should be in this channel.
-        ags_region = [convert_phone_number(ag["Phone number"]) for ag in ags if converter(MUNICIPALITY, REGION, ag["Municipality"]) == region]
-        ags_in_channel = ["+" + user.phone for user in await client.get_participants(links[region])]
+        ags_region = [(convert_phone_number(ag["Phone number"]), ag["rep_name"]) for ag in ags if converter(MUNICIPALITY, REGION, ag["Municipality"]) == region]
+        ags_in_channel = ["+" + user.phone for user in await client.get_participants(links[region]) if user.phone]
 
         # Find the people who should be added/removed.
-        to_add = [ag for ag in ags_region if ag not in ags_in_channel]
+        to_add = [ag for ag in ags_region if ag[0] not in ags_in_channel]
+        already_in = [ag for ag in ags_region if ag[0] in ags_in_channel]
+
+        print(region)
+        print("an: {}".format(ags_region))
+        print("telegram: {}".format(ags_in_channel))
+        print("add: {}".format(to_add))
+        print("already in: {}".format(already_in))
 
         # Add users and log the result.
         for ag in to_add:
@@ -94,12 +103,14 @@ async def sync_channels(api_key, links, super_admins, an_ag_endpoints):
                     logs[region]["update_number"].append(ag)
                 else: # User was added.
                     logs[region]["added"].append(ag)
+                    await send_invite(ag, region, links)
             except UserNotMutualContactError: # User not a mut. contact.
                 if not ag in logs[region]["error"]:
                     logs[region]["error"].append(ag)
             except IndexError: # User doesn't have telegram.
                 if not ag in logs[region]["no_telegram"]:
                     logs[region]["no_telegram"].append(ag)
+
 
 
 async def send_log(admins):
@@ -118,6 +129,7 @@ async def send_log(admins):
         message += "\n      Toegevoegd: {}".format(logs[region]["added"])
         message += "\n      Geen telegram: {}".format(logs[region]["no_telegram"])
         message += "\n      Error bij toevoegen: {}".format(logs[region]["error"])
+        message += "\n      Update nummer: {}".format(logs[region]["update_number"])
 
     for admin in admins:
         await client.send_message(admin, message)
@@ -137,18 +149,21 @@ async def send_invite(user, region, links):
         Sends a bilingual message to the specified 'user' to invite them to the
         AG rep telegram channel corresponding to the 'region' specified.
     """
-    message = "Hoi, \nJij staat in Action Network geregistreerd als de afgevaartdige/representative van je Extinction Rebellion affiniteitsgroep. Zojuist ben je (als het goed is) toegevoegd aan het officiële telegram kanaal voor affiniteitsgroepen in regio {}. Dit kanaal houdt je up-to-date over alle acties in je regio, en geeft andere informatie die je affiniteitsgroep helpt bij het actie voeren. Door de informatie die hier langs komt in de gaten te houden, en door te geven aan je affiniteitsgroep, weet je alles wat nodig is om in actie te komen met Extinction Rebellion! \nMeer informatie over de kanalen vind je hier: tinyurl.com/ag-kanalen . Mochten ook andere leden van je affiniteitsgroep in het kanaal willen, dan is hier een link waarmee ze toegang krijgen: {} . Mocht jullie groep in meerdere regio's actief zijn, stuur mij dan even een berichte - ik kan je dan ook in de kanelen voor andere regio's plaatsen. Voor overige vragen kun je mij hier ook via Telegram contacteren.\nLove & Rage Martijn van Extinction Rebellion".format(region, links[region])
-    message_en = "Hi,\nYou're registered as the representative of your Extinction Rebellion affinity group through Action Network. If all went well, you were just added to the official information channel for affinity groups in your region: {}. This channel will keep you up to date on all actions in your region, as well as other information that is useful to support your affinity group. By keeping an eye on the information that comes through here, and passing it on to your affinity group, you will know everything you need to come into action with XR!\nMore information about the channels can be found here: tinyurl.com/ag-kanalen   If other members of your affinity group also wish to join the channel, here's a link to give them access: {} . If your affinity group is active in mulitple reagions, please send me a message, and I will add you to the channels for the other regions as well. You can also reach me here on telegram for any addition questions!\nLove & Rage Martijn from Extinction Rebellion".format(region, links[region])
+    # Format the messages.
+    message = "Beste {}, \nJij staat in Action Network geregistreerd als de afgevaardigte/representative van je Extinction Rebellion affiniteitsgroep. Zojuist ben je toegevoegd aan het officiële telegram kanaal voor affiniteitsgroepen in regio {}. Dit kanaal houdt je up-to-date over alle acties in je regio, en geeft andere informatie die je affiniteitsgroep helpt bij het actie voeren. Door de informatie die hier langs komt in de gaten te houden, en door te geven aan je affiniteitsgroep, weet je alles wat nodig is om in actie te komen met Extinction Rebellion! \nMeer informatie over de kanalen vind je hier: tinyurl.com/ag-kanalen . Mochten ook andere leden van je affiniteitsgroep in het kanaal willen, dan is hier een link waarmee ze toegang krijgen: {} . Mocht jullie groep in meerdere regio's actief zijn, stuur mij dan even een berichte - ik kan je dan ook in de kanelen voor andere regio's plaatsen. Voor overige vragen kun je mij hier ook via Telegram contacteren.\nLove & Rage Martijn van Extinction Rebellion".format(user[1], region, links[region])
+    message_en = "Dear {},\nYou're registered as the representative of your Extinction Rebellion affinity group through Action Network. If all went well, you were just added to the official information channel for affinity groups in your region: {}. This channel will keep you up to date on all actions in your region, as well as other information that is useful to support your affinity group. By keeping an eye on the information that comes through here, and passing it on to your affinity group, you will know everything you need to come into action with XR!\nMore information about the channels can be found here: tinyurl.com/ag-kanalen   If other members of your affinity group also wish to join the channel, here's a link to give them access: {} . If your affinity group is active in mulitple reagions, please send me a message, and I will add you to the channels for the other regions as well. You can also reach me here on telegram for any addition questions!\nLove & Rage Martijn from Extinction Rebellion".format(user[1], region, links[region])
+
+    # Send them.
     try:
-        await client.send_message(user, message)
-        await client.send_message(user, message_en)
+        await client.send_message(user[0], message_en)
+        await client.send_message(user[0], message)
     except UserIdInvalidError and ValueError as e:
         print("Invalid phonenumber - either the AG rep has not installed telegram, or the phone number is invalid. User: {}    Error: '{}'".format(user, e))
 
 
 async def add_user(user, region, links):
     # Add user to contact. This allows us to add them to the channel
-    contact = InputPhoneContact(client_id=0, phone=user, first_name="xr_ag_rep", last_name="{}".format(region))
+    contact = InputPhoneContact(client_id=0, phone=user[0], first_name=user[1], last_name="xr_ag_rep_{}".format(region))
     try:
         result = await client(ImportContactsRequest([contact]))
     except FloodWaitError as e:
