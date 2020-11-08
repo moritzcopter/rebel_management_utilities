31e3221435fc3a48df646858bc797c11d6bc4fae
Moritz Burghardt <pm.burghardt@tum.de>
2020-11-08 13:44:10 +0100

    Except per local group

--- local_group_support/update_integrator_spreadsheets.py
+++ local_group_support/update_integrator_spreadsheets.py
@@ -6,11 +6,11 @@ from local_group_support.utils.nextcloud import get_nextcloud_user, BASE_URL, IN
     write_to_spreadsheet
 
 if __name__ == "__main__":
-    try:
-        start_date = datetime.date.today() - datetime.timedelta(days=10)
-        df = get_member_stats(start_date)
+    start_date = datetime.date.today() - datetime.timedelta(days=30)
+    df = get_member_stats(start_date)
 
-        for local_group, df_grouped in df.groupby('local_group'):
+    for local_group, df_grouped in df.groupby('local_group'):
+        try:
             filename = f'New rebels {local_group}.xlsx'
             username = get_nextcloud_user()
             local_group_safe = local_group.replace('/', '+')
@@ -20,7 +20,6 @@ if __name__ == "__main__":
                  'taggings']].set_index('submission_date').sort_index()
             df_formatted['next_action'] = 'Contact'
             write_to_spreadsheet(url, df_formatted, deduplicate_column='email_address')
-
-        post_to_channel(LOGGING_CHANNEL_ID, 'Successfully updated integrator spreadsheets')
-    except Exception as e:
-        post_to_channel(LOGGING_CHANNEL_ID, f'@all Failed to update integrator spreadsheet - {e}')
+            post_to_channel(LOGGING_CHANNEL_ID, f'Successfully updated integrator spreadsheet for {local_group}')
+        except Exception as e:
+            post_to_channel(LOGGING_CHANNEL_ID, f'@all Failed to update integrator spreadsheet for {local_group} - {e}')
